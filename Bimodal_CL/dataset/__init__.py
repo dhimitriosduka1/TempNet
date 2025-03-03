d4a67eca1409d79f96ab79e3889f791c8b93a736
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-03-03 17:28:34 +0100

    Add tau_min and tau_max to make_dataset_train

--- Bimodal_CL/dataset/__init__.py
+++ Bimodal_CL/dataset/__init__.py
@@ -84,7 +84,9 @@ def create_train_dataset(dataset, args, use_test_transform=False):
     return make_dataset_train(
         transform=train_transform,
         batch_size=args.batch_size_train,
-        return_key=True
+        return_key=True,
+        tau_min=args.pct_tau_min,
+        tau_max=args.pct_tau_max
     )
 
     # DD
