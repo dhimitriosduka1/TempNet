8a143c6983ff3992e9aff5d88035bc4d399ed679
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-03-11 12:10:17 +0100

    Refactor submit file

---
+++ Bimodal_CL/submit/dhimitrios/clip_tau_0.01_lr_8e-4/clip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu22
+
+#SBATCH --time=35:59:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+DATA_PATH=.
+DATA=cc3m
+LR=8e-4
+ITA_TYPE=clip
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --run_name BASELINE_CLIP \
+    --data_path $DATA_PATH \
+    --data $DATA \
+    --output_dir /BS/dduka/work/projects/TempNet/Bimodal_CL/submit/dhimitrios/clip_tau_0.01_lr_8e-4 \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type $ITA_TYPE \
