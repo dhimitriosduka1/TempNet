199e2f8bf0bb9ce1e7ccac05bdf8e71e76e38fdd
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-03-11 12:17:21 +0100

    Add scripts for experiments

---
+++ Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.07_lr_8e-4/clip_cos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu22
+
+#SBATCH --time=05:59:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+DATA_PATH=.
+DATA=cc3m
+LR=8e-4
+TAU_MIN=0.01
+TAU_MAX=0.07
+
+DESC=BASELINE_CLIP_COS_${TAU_MIN}_${TAU_MAX}_TEST
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --run_name $DESC \
+    --data_path $DATA_PATH \
+    --data $DATA \
+    --output_dir /BS/dduka/work/projects/TempNet/Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.07_lr_8e-4 \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type clip \
+    --temperature_scheduler cos \
+    --tau_min $TAU_MIN \
+    --tau_max $TAU_MAX \
