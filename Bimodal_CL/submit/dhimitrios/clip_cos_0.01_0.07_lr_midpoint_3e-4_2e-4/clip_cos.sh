b3b9c0e567a123853d963cdf2e6be12219c44759
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-03-12 13:10:35 +0100

    Add submit script with midpoint scheduler

---
+++ Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.07_lr_midpoint_3e-4_2e-4/clip_cos.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu20
+
+#SBATCH --time=01:59:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+PROJECT_DIR="/BS/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+DATA_PATH=.
+DATA=cc3m
+LR=2e-4
+
+TAU_MIN=0.01
+TAU_MAX=0.07
+
+LR_START=3e-4
+LR_END=2e-4
+
+DESC=BASELINE_CLIP_COS_${TAU_MIN}_${TAU_MAX}_LR_MINDPOINT_${LR_START}_${LR_END}
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --run_name $DESC \
+    --data_path $DATA_PATH \
+    --data $DATA \
+    --output_dir /BS/dduka/work/projects/TempNet/Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.07_lr_midpoint_3e-4_2e-4/clip_cos.sh \
+    --init_model \
+    --use_amp \
+    --epochs 10 --lr $LR \
+    --ita_type clip \
+    --temperature_scheduler cos \
+    --tau_min $TAU_MIN \
+    --tau_max $TAU_MAX \
+    --sched midpoint \
+    --lr_start $LR_START \
+    --lr_end $LR_END \
