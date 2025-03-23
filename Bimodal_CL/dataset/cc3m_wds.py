f7ca537d8e405737c340b85ede214829211e4503
Dhimitrios Duka <111680669+dhimitriosduka1@users.noreply.github.com>
2025-03-23 20:15:03 +0100

    Update cc3m_wds.py | Add non-wds cc3m dataset

--- Bimodal_CL/dataset/cc3m_wds.py
+++ Bimodal_CL/dataset/cc3m_wds.py
@@ -1,12 +1,14 @@
 import io
 import os
+import json
 import torch
+import pickle
 import webdataset as wds
 from dataset.utils import pre_caption
 import PIL.Image as Image
 import torch.distributed as dist
+from torch.utils.data import Dataset
 
-import pickle
 from scheduler.temperature_scheduler import get_per_class_temperature
 
 
@@ -26,14 +28,30 @@ def get_world_size():
     return dist.get_world_size()
 
 
-def get_dataset_size():
-    # https://github.com/AILab-CVC/SEED/blob/93b3cf408196735ec4820ad2eb4d9dc4a670003d/MultiModalLLM/src/data/data.py#L73C1-L74C1
+def get_train_dataset_size():
+    # Value we got after counting the number of samples in the training dataset
     return 2905954
 
 
-def get_shard_list(
-    start_index=0, end_index=110, base_path="/BS/databases23/CC3M_tar/training/"
-):
+def get_val_dataset_size():
+    # Value we got after counting the number of samples in the validation dataset
+    return 13358
+
+
+def get_train_shards():
+    return _get_shard_list(
+        start_index=0, end_index=110, base_path="/BS/databases23/CC3M_tar/training/"
+    )
+
+
+def get_val_shards():
+    # We only have on validation shard. Therefore, start_index and end_index are the same.
+    return _get_shard_list(
+        start_index=0, end_index=0, base_path="/BS/databases23/CC3M_tar/validation"
+    )
+
+
+def _get_shard_list(start_index, end_index, base_path):
     shard_list = []
     for i in range(start_index, end_index + 1):
         tar_file_path = os.path.join(base_path, f"{i}.tar")
@@ -64,7 +82,7 @@ def make_dataset_train(
             return pickle.load(file)
 
     # TODO: Change `torch.tensor(-1.0)` to correct values when using different loss than CLIP
-    def make_sample(sample, precomputed_classes, per_class_temperature):
+    def make_sample_train(sample, precomputed_classes, per_class_temperature):
         image = sample["image.pth"]
         caption = sample["metadata.pyd"]["caption"]
         key = sample["__key__"]
@@ -82,7 +100,7 @@ def make_dataset_train(
         )
 
     train_set = wds.WebDataset(
-        urls=get_shard_list(),
+        urls=get_train_shards(),
         resampled=True,
         shardshuffle=True,
         cache_dir=cache_dir,
@@ -91,20 +109,47 @@ def make_dataset_train(
 
     precomputed_classes = load_precomputed_classes()
     per_class_temperature = get_per_class_temperature(
-        classes_=precomputed_classes["metadata"]["classes"], tau_min=tau_min, tau_max=tau_max
+        classes_=precomputed_classes["metadata"]["classes"],
+        tau_min=tau_min,
+        tau_max=tau_max,
     )
-    
+
     train_set = (
         train_set.shuffle(1000)
         .decode(decoder_pth)
         .map(
-            lambda sample: make_sample(
+            lambda sample: make_sample_train(
                 sample, precomputed_classes, per_class_temperature
             )
         )
     )
     train_set = train_set.batched(batch_size)
     return train_set
+
+
+def make_dataset_val(transform, max_words=30, batch_size=128):
+    def make_sample_val(sample):
+        image = sample["image.pth"]
+        caption = sample["metadata.pyd"]["caption"]
+        key = sample["__key__"]
+
+        return (
+            transform(image),
+            pre_caption(caption=caption, max_words=max_words),
+            key,
+            # Placeholder for class
+            torch.tensor(-1.0),
+        )
+
+    return (
+        wds.WebDataset(
+            urls=get_val_shards(),
+            nodesplitter=wds.split_by_worker,
+        )
+        .decode(decoder_pth)
+        .map(make_sample_val)
+        .batched(batch_size)
+    )
 
 
 def make_dataloader_train(trainset, batch_size=128, num_workers=4):
@@ -115,8 +160,65 @@ def make_dataloader_train(trainset, batch_size=128, num_workers=4):
 
     # A resampled dataset is infinite size, but we can recreate a fixed epoch length.
     world_size = get_world_size()
-    batches_per_epoch = get_dataset_size() // (batch_size * world_size)
+    batches_per_epoch = get_train_dataset_size() // (batch_size * world_size)   
     trainloader.batches_per_epoch = batches_per_epoch
     trainloader = trainloader.with_epoch(batches_per_epoch)
 
     return trainloader
+
+
+def make_dataloader_val(valset, batch_size=128):
+    # In a IterableDataset, the batch creation is done in the dataset
+    dataloader = wds.WebLoader(valset, batch_size=None, num_workers=4, shuffle=False)
+    
+    world_size = get_world_size()
+    batches_per_epoch = get_val_dataset_size() // (batch_size * world_size)
+    dataloader = dataloader.with_epoch(batches_per_epoch)
+
+    # Add mappings for evaluation
+    size = get_val_dataset_size()
+    dataloader.dataset = {
+        "txt2img": {i: [i] for i in range(size)},
+        "img2txt": {i: [i] for i in range(size)},
+    }
+
+    return dataloader
+
+# A non-wds version of the validation dataset
+class CC3M_Val_Dataset(Dataset):
+    def __init__(self, ann_file, transform, root, max_words=30):
+        self.ann = json.load(open(ann_file, "r"))
+        self.transform = transform
+        self.root = root
+        self.max_words = max_words
+
+        self.text = []
+        self.image = []
+        self.txt2img = {}
+        self.img2txt = {}
+
+        txt_id = 0
+        for img_id, ann in enumerate(self.ann):
+            self.image.append(ann["image"])
+            self.img2txt[img_id] = []
+
+            # Preprocess caption
+            caption_path = os.path.join(self.root, ann["caption"])
+            caption = pickle.load(open(caption_path, "rb"))["caption"]
+            caption = pre_caption(caption, self.max_words)
+
+            self.text.append(caption)
+
+            self.img2txt[img_id].append(txt_id)
+            self.txt2img[txt_id] = img_id
+            txt_id += 1
+
+    def __len__(self):
+        return len(self.ann)
+
+    def __getitem__(self, index):
+        image_path = os.path.join(self.root, self.ann[index]["image"])
+        image = torch.load(image_path, map_location="cpu", weights_only=True)
+        image = Image.fromarray(image.numpy()).convert("RGB")
+        image = self.transform(image)
+        return image, index
