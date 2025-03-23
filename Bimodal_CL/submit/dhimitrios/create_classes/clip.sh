0fa4764e61a583dacba7119260f4a7f382a75f31
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-03-23 23:47:21 +0100

    Add script for creating classes

---
+++ Bimodal_CL/submit/dhimitrios/create_classes/clip.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu22
+
+#SBATCH --time=00:59:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+PROJECT_DIR="/BS/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+DATA_PATH=.
+DATA=cc3m
+LR=8e-4
+ITA_TYPE=clip
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --run_name BASELINE_CLIP_RESUMED___ \
+    --data_path $DATA_PATH \
+    --data $DATA \
+    --output_dir /BS/dduka/work/projects/TempNet/Bimodal_CL/submit/dhimitrios/create_classes/ \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type $ITA_TYPE \
+    --find_clusters \
+    --num_clusters 18 \
