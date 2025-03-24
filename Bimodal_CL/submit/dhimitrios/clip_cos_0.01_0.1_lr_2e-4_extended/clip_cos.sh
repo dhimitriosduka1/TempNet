19e16eed797fc1df2de864fe4775fcef1344e3ea
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-03-24 01:03:23 +0100

    WIP

---
+++ Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.1_lr_2e-4_extended/clip_cos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu22
+
+#SBATCH --time=01:59:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+PROJECT_DIR="/BS/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+DATA_PATH=.
+DATA=cc3m
+LR=2e-4
+TAU_MIN=0.01
+TAU_MAX=0.1
+
+DESC=BASELINE_CLIP_COS_${TAU_MIN}_${TAU_MAX}_LR_${LR}_EXTENDED
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --run_name $DESC \
+    --data_path $DATA_PATH \
+    --data $DATA \
+    --output_dir /BS/dduka/work/projects/TempNet/Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.1_lr_2e-4_extended/ \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type clip \
+    --temperature_scheduler cos \
+    --tau_min $TAU_MIN \
+    --tau_max $TAU_MAX \
+    --cc3m_extended_captions_path /BS/dduka/work/databases/cc3m/train/captions_extended_llm.json \
