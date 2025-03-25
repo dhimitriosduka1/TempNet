d885824da1f5d2e9455c8030412f63b5f2950928
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-03-25 14:32:18 +0100

    Add script for baseline clip with extended captions

---
+++ Bimodal_CL/submit/dhimitrios/clip_tau_0.01_lr_8e-4_extended/clip.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu22
+
+#SBATCH --time=07:59:00
+#SBATCH -a 1-6%1
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+PROJECT_DIR="/BS/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+DATA_PATH=.
+DATA=cc3m
+LR=8e-4
+ITA_TYPE=clip
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --run_name BASELINE_CLIP_EXTENDED \
+    --data_path $DATA_PATH \
+    --data $DATA \
+    --output_dir /BS/dduka/work/projects/TempNet/Bimodal_CL/submit/dhimitrios/clip_tau_0.01_lr_8e-4_extended/ \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type $ITA_TYPE \
+    --cc3m_extended_captions_path /BS/dduka/work/databases/cc3m/train/captions_extended_llm.json \
