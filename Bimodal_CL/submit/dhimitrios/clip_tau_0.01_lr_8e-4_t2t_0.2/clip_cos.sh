9da78b721b97585bdee15a5f0940ccd4166c2289
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-04-01 15:57:07 +0200

    Add image augmentation examples

---
+++ Bimodal_CL/submit/dhimitrios/clip_tau_0.01_lr_8e-4_t2t_0.2/clip_cos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu22
+
+#SBATCH --time=11:59:00
+#SBATCH -a 1-4%1
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+PROJECT_DIR="/BS/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+DATA_PATH=.
+DATA=cc3m
+LR=8e-4
+
+LOSS_WEIGHT=0.2
+
+DESC=BASELINE_CLIP_T2T_${LOSS_WEIGHT}
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4825 \
+    --use_env clip.py \
+    --run_name $DESC \
+    --data_path $DATA_PATH \
+    --data $DATA \
+    --output_dir /BS/dduka/work/projects/TempNet/Bimodal_CL/submit/dhimitrios/clip_tau_0.01_lr_8e-4_t2t_0.2/ \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type clip \
+    --enable_t2t_loss \
+    --t2t_loss_weight $LOSS_WEIGHT \
+    --cc3m_extended_captions_path /BS/dduka/work/databases/cc3m/train/captions_extended_llm.json \
