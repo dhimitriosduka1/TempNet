2082a0458854b4bed9d8bf6b74a5276e1e5dfc52
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-04-11 16:38:29 +0200

    Rename files

--- Bimodal_CL/submit/dhimitrios/raven/r_scheduled_clip_0.01_0.05_lr_2e-4_quad/submit.sh
+++ Bimodal_CL/submit/dhimitrios/raven/r_scheduled_clip_0.01_0.05_lr_2e-4_quad/submit.sh
@@ -31,13 +31,13 @@ ITA_TYPE=scheduled_clip_loss
 BASE_TAU=0.01
 ALPHA=0.05
  
-DESC=R_SCHEDULED_CLIP_${BASE_TAU}_${ALPHA}_${LR}
+DESC=R_SCHEDULED_CLIP_${BASE_TAU}_${ALPHA}_${LR}_QUAD
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
     --use_env clip.py \
     --run_name $DESC \
     --data $DATA \
-    --output_dir /ptmp/dduka/work/training_metadata/bimodal_cl/dhimitrios/r_scheduled_clip_0.01_0.05_lr_2e-4/ \
+    --output_dir /ptmp/dduka/work/training_metadata/bimodal_cl/dhimitrios/r_scheduled_clip_0.01_0.05_lr_2e-4_quad/ \
     --init_model \
     --use_amp \
     --epochs 30 --lr $LR \
