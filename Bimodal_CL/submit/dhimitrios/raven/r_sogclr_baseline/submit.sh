51589fa0e434bc2ca8d221cf841185e3108b9274
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-04-20 23:51:59 +0200

    Sogclr for raven

---
+++ Bimodal_CL/submit/dhimitrios/raven/r_sogclr_baseline/submit.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+#SBATCH -o /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+#SBATCH --job-name bcl
+
+#SBATCH --ntasks=1
+#SBATCH --constraint="gpu"
+
+#SBATCH --gres=gpu:4
+#SBATCH --mem=480000
+
+#SBATCH --time=11:59:59
+#SBATCH --array=1-6%1
+
+module purge
+module load anaconda/3/2023.03
+
+conda activate bimodal_cl
+
+export mpcdf=1
+
+PROJECT_DIR="/u/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+DATA=cc3m
+LR=8e-4
+
+GAMMA=0.8
+
+DESC=SOGCLR_BASELINE
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4821 \
+    --use_env clip.py \
+    --run_name $DESC \
+    --data $DATA \
+    --output_dir /BS/dduka/work/training_metadata/bimodal_cl/dhimitrios/sogclr_baseline/ \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type sogclr \
+    --sogclr_gamma $GAMMA \
