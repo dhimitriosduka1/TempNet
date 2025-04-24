6d768530ac0c36cb3b2a3459c638eae7778ba5b5
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-04-25 01:19:04 +0200

    WIP

---
+++ Bimodal_CL/submit/dhimitrios/raven/r_extract_vision_features_dinov2_large/submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+#SBATCH -o /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+#SBATCH --job-name bcl
+
+#SBATCH --ntasks=1
+#SBATCH --constraint="gpu"
+
+#SBATCH --gres=gpu:4
+#SBATCH --mem=480000
+
+#SBATCH --time=11:59:59
+
+module purge
+module load anaconda/3/2023.03
+
+conda activate bimodal_cl
+
+export mpcdf=1
+
+PROJECT_DIR="/u/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+accelerate launch /submit/dhimitrios/raven/r_extract_vision_features_dinov2_large/submit.sh \
+    --tar-file /ptmp/dduka/work/data/cc3m/training \
+    --output-dir /ptmp/dduka/work/data/cc3m/training_features_dinov2 \
