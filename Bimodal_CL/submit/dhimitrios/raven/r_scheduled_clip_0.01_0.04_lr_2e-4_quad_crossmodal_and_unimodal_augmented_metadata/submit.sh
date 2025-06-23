0f8bf0437e0e1a886d0e484c0518dd0e4f3718c7
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-06-23 23:37:02 +0200

    .

--- Bimodal_CL/submit/dhimitrios/raven/r_scheduled_clip_0.01_0.04_lr_2e-4_quad_crossmodal_and_unimodal_augmented_metadata/submit.sh
+++ Bimodal_CL/submit/dhimitrios/raven/r_scheduled_clip_0.01_0.04_lr_2e-4_quad_crossmodal_and_unimodal_augmented_metadata/submit.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
+#!/bin/bash -l
 
-#SBATCH --job-name cc3m
-#SBATCH --partition gpu22
+#SBATCH -o /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
 
-#SBATCH --time=05:59:00
-#SBATCH -a 1-8%1
+#SBATCH --job-name bcl
 
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:a100:4
+#SBATCH --ntasks=1
+#SBATCH --constraint="gpu"
 
-#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
-#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+#SBATCH --gres=gpu:4
+#SBATCH --mem=480000
 
-export mpi=1
-PROJECT_DIR="/BS/dduka/work/projects/TempNet/Bimodal_CL"
+#SBATCH --time=11:59:59
+#SBATCH --array=1-3%1
+
+module purge
+module load anaconda/3/2023.03
+
+conda activate bimodal_cl
+
+export mpcdf=1
+
+PROJECT_DIR="/u/dduka/work/projects/TempNet/Bimodal_CL"
 cd "${PROJECT_DIR}"
 
 DATA=cc3m
@@ -30,7 +37,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_nod
     --use_env clip.py \
     --run_name $DESC \
     --data $DATA \
-    --output_dir /BS/dduka/work/training_metadata/bimodal_cl/dhimitrios/$DESC/ \
+    --output_dir /ptmp/dduka/work/training_metadata/bimodal_cl/dhimitrios/$DESC/ \
     --init_model \
     --use_amp \
     --epochs 30 --lr $LR \
@@ -40,4 +47,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_nod
     --clip_scheduled_loss_type quadratic \
     --enable_i2i_loss \
     --enable_t2t_loss \
-    --cc3m_extended_captions_path /BS/dduka/work/databases/cc3m/train/captions_extended_llm.json \
+    --cc3m_extended_captions_path /ptmp/dduka/work/data/cc3m/training/captions_extended_llm.json \
