67dd9343f2b2da873fc23fa0846c701d0a8c33c9
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-06-26 16:32:44 +0200

    Scripts

--- Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.05_lr_8e-4_vitb16/clip_cos.sh
+++ Bimodal_CL/submit/dhimitrios/clip_cos_0.01_0.05_lr_8e-4_vitb16/clip_cos.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name cc3m
-#SBATCH --partition gpu24
+#SBATCH --partition gpu22
 
-#SBATCH --time=11:59:00
+#SBATCH --time=05:59:00
 #SBATCH -a 1-4%1
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:h100:4
+#SBATCH --gres=gpu:a100:4
 
 #SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
 #SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
