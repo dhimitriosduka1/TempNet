70e2e1a74cc8aed49c8e532c417ca45e6602a9b2
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-06-27 16:17:50 +0200

    Scritp

--- Bimodal_CL/submit/find_clusters.sh
+++ Bimodal_CL/submit/find_clusters.sh
@@ -1,27 +1,21 @@
-#!/bin/bash -l
+#!/bin/bash
 
-#SBATCH -o /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
-#SBATCH -e /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+#SBATCH --job-name cc3m
+#SBATCH --partition gpu20
 
-#SBATCH --job-name bcl
+#SBATCH --time=11:59:00
 
-#SBATCH --ntasks=1
-#SBATCH --constraint="gpu"
-
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4
-#SBATCH --mem=480000
 
-#SBATCH --time=23:59:59
+#SBATCH -o /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /BS/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
 
-module purge
-module load anaconda/3/2023.03
-
-conda activate bimodal_cl
-
-export mpcdf=1
-
-PROJECT_DIR="/u/dduka/work/projects/TempNet/Bimodal_CL"
+export mpi=1
+PROJECT_DIR="/BS/dduka/work/projects/TempNet/Bimodal_CL"
 cd "${PROJECT_DIR}"
+
 
 DATA=cc3m
 LR=8e-4
