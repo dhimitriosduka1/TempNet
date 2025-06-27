38764468582817a3918809e987f6d48195b3d6d8
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-06-27 09:12:24 +0200

    Scripts

---
+++ Bimodal_CL/submit/isogclr_tempnet_lr_2e-4_lr_tempnet_1e-6.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+#SBATCH -o /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+#SBATCH --job-name bcl
+
+#SBATCH --ntasks=1
+#SBATCH --constraint="gpu"
+
+#SBATCH --gres=gpu:4
+#SBATCH --mem=480000
+
+#SBATCH --time=23:59:59
+
+module purge
+module load anaconda/3/2023.03
+
+conda activate bimodal_cl
+
+export mpcdf=1
+
+PROJECT_DIR="/u/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+data=cc3m
+
+lr=2e-4
+lr_temp_net=1e-6
+desc=isogclr_tempnet_lr_${lr}_lr_temp_net_${lr_temp_net}_M256_pt_dataloader
+gamma=0.8
+rho=7.0
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --data ${data} \
+    --output_dir /ptmp/dduka/work/training_metadata/bimodal_cl/dhimitrios/$desc \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr ${lr} \
+    --lr_temp_net ${lr_temp_net} \
+    --rho ${rho} \
+    --ita_type isogclr_tempnet \
+    --sogclr_gamma ${gamma} \
+    --batch_size_train 256 \
+    --run_name $desc
