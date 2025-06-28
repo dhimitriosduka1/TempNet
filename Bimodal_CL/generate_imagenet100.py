8345d4db700514e8cf4edcb9538dede74bc0ec5c
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-06-28 17:51:57 +0200

    Add generate imagenet100 script

---
+++ Bimodal_CL/generate_imagenet100.py
@@ -0,0 +1,51 @@
+# Source: https://github.com/danielchyeh/ImageNet-100-Pytorch/blob/main/generate_IN100.py
+import os
+import shutil
+import argparse
+import json
+
+
+def parse_option():
+    parser = argparse.ArgumentParser("argument for generating ImageNet-100")
+
+    parser.add_argument(
+        "--source_folder", type=str, default="", help="folder of ImageNet-1K dataset"
+    )
+    parser.add_argument(
+        "--target_folder", type=str, default="", help="folder of ImageNet-100 dataset"
+    )
+    parser.add_argument(
+        "--target_class",
+        type=str,
+        default="/BS/dduka/work/projects/TempNet/Bimodal_CL/dataset/Labels.json",
+        help="class file of ImageNet-100",
+    )
+
+    opt = parser.parse_args()
+
+    return opt
+
+
+def generate_data(source_folder, target_folder, target_class):
+    # Create target folder if it doesn't exist
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    with open(target_class, "r") as f:
+        labels = json.load(f)
+
+    f = []
+    for key, _ in labels.items():
+        f.append(key)
+
+    for ids, dirs in enumerate(os.listdir(source_folder)):
+        # Check if the directory is in the labels
+        if dirs in f:
+            print("{} is transferred".format(dirs))
+            shutil.copytree(
+                os.path.join(source_folder, dirs), os.path.join(target_folder, dirs)
+            )
+
+
+opt = parse_option()
+generate_data(opt.source_folder, opt.target_folder, opt.target_class)
