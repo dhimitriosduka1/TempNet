8aa19bab76258e9de38cefa5c6df92a57d569614
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-06-28 21:00:20 +0200

    Remove configs

--- Bimodal_CL/submit/dhimitrios/raven/imagenet100/clip_cos_0.01_0.05_lr_8e-4/clip_cos.sh
+++ Bimodal_CL/submit/dhimitrios/raven/imagenet100/clip_cos_0.01_0.05_lr_8e-4/clip_cos.sh
@@ -29,7 +29,7 @@ LR=8e-4
 TAU_MIN=0.01
 TAU_MAX=0.05
 
-DESC=BASELINE_CLIP_COS_${TAU_MIN}_${TAU_MAX}_IMAGENET100
+DESC=R_BASELINE_CLIP_COS_${TAU_MIN}_${TAU_MAX}_IMAGENET100
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
     --use_env clip.py \
@@ -43,5 +43,3 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_nod
     --temperature_scheduler cos \
     --tau_min $TAU_MIN \
     --tau_max $TAU_MAX \
-    --image_encoder vit_base_patch16_224 \
-    --image_res 224 \
