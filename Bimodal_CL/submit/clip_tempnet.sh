11a3a23a32576675d82cbd5bbc7bc67aa1153d87
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-06-30 20:57:23 +0200

    Add clip_tempnet script

---
+++ Bimodal_CL/submit/clip_tempnet.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+
+#SBATCH -o /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+#SBATCH --job-name bcl
+
+#SBATCH --ntasks=1
+#SBATCH --constraint="gpu"
+
+#SBATCH --gres=gpu:4
+#SBATCH --mem=480000
+
+#SBATCH --time=23:59:59
+
+module purge
+module load anaconda/3/2023.03
+
+conda activate bimodal_cl
+
+export mpcdf=1
+
+PROJECT_DIR="/u/dduka/work/projects/TempNet_/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+data=cc3m
+
+lr=2e-4
+lr_temp_net=1e-6
+desc=CLIP_TEMPNET_LR_${lr}_LR_TEMP_NET_${lr_temp_net}
+rho=7.0
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --data ${data} \
+    --output_dir /ptmp/dduka/work/training_metadata/bimodal_cl/dhimitrios/$desc \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr ${lr} \
+    --lr_temp_net ${lr_temp_net} \
+    --rho ${rho} \
+    --ita_type clip_tempnet \
+    --batch_size_train 256 \
+    --run_name $desc
