b5a40e7a2cb1a7d8c93fd433453190e7c588f299
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-07-01 07:32:36 +0200

    Debug

--- Bimodal_CL/submit/clip_tempnet_debug.sh
+++ Bimodal_CL/submit/clip_tempnet_debug.sh
@@ -24,8 +24,8 @@ cd "${PROJECT_DIR}"
 
 data=cc3m
 
-lr=8e-4
-desc=clip_tempnet_lr8e-4_M256_pt_dataloader_debug_6
+lr=5e-5
+desc=clip_tempnet_lr5e-5_M256_pt_dataloader_debug_7
 rho=7.0
 
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4820 \
