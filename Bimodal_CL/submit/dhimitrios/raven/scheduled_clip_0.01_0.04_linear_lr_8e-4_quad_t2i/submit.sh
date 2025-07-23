80586d465b98eab7ea34d98f329e379e9616e77a
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-07-24 00:50:41 +0200

    Add runs

---
+++ Bimodal_CL/submit/dhimitrios/raven/scheduled_clip_0.01_0.04_linear_lr_8e-4_quad_t2i/submit.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -l
+
+#SBATCH -o /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/bimodal_cl/%A_%a_%x_%j_%N.err
+
+#SBATCH --job-name bcl
+
+#SBATCH --ntasks=1
+#SBATCH --constraint="gpu"
+
+#SBATCH --gres=gpu:4
+#SBATCH --mem=480000
+
+#SBATCH --time=11:59:59
+#SBATCH --array=1-6%1
+
+module purge
+module load anaconda/3/2023.03
+
+conda activate bimodal_cl
+
+export mpcdf=1
+
+PROJECT_DIR="/u/dduka/work/projects/TempNet/Bimodal_CL"
+cd "${PROJECT_DIR}"
+
+DATA=cc3m
+LR=8e-4
+ITA_TYPE=scheduled_clip_loss
+
+BASE_TAU=0.01
+ALPHA=0.04
+
+DESC=SCHEDULED_CLIP_${BASE_TAU}_${ALPHA}_LINEAR_${LR}_QUAD_T2I
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=4820 \
+    --use_env clip.py \
+    --run_name $DESC \
+    --data $DATA \
+    --output_dir /BS/dduka/work/training_metadata/bimodal_cl/dhimitrios/scheduled_clip_0.01_0.04_linear_lr_8e-4_quad_t2i/ \
+    --init_model \
+    --use_amp \
+    --epochs 30 --lr $LR \
+    --ita_type $ITA_TYPE \
+    --sim_based_loss_alpha $ALPHA \
+    --temp $BASE_TAU \
+    --sched linear \
+    --per_sample_temp_similarity t2i \
+    --per_sample_temp_mapping adaptive_with_base \
+    --clip_scheduled_loss_type quadratic \
